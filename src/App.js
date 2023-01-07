f34b796491b668a6c285af34c46c0d95b477984b
DubDub Production <leo.wong@leowong.co>
2023-01-08 06:43:57 +0800

    [add] Update Function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import React, { Suspense } from "react";
 import { PageLoading } from "./PageLoading";
 import { app } from "./component/firebase.js";
 import { getAnalytics } from "firebase/analytics";
+import appInfo from "../package.json";
+import {
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 
 const LRT = React.lazy(() => import("./LRT"));
 const MTR = React.lazy(() => import("./MTR"));
@@ -16,6 +23,20 @@ const HKTram = React.lazy(() => import("./HKTram"));
 const MTRBus = React.lazy(() => import("./MTRBus"));
 const WeatherBanner = React.lazy(() => import("./component/HK_weather"));
 
+const storage = window.localStorage;
+const version = storage.getItem("appVersion");
+
+if (storage.getItem("appVersion") === null) {
+  storage.setItem("appVersion", appInfo.version);
+}
+
+if (version < appInfo.version) {
+  setTimeout(() => {
+    storage.setItem("appVersion", appInfo.version);
+    window.location.reload();
+  }, 3000);
+}
+
 function App() {
   getAnalytics(app);
   return (
@@ -26,6 +47,18 @@ function App() {
             name="viewport"
             content="width=device-width, maximum-scale=1.0, user-scalable=0"
           />
+          {version!== null &&version < appInfo.version ? (
+            <Dialog maxWidth="md" open>
+              <DialogTitle id="confirmation-dialog-title">更新 Update</DialogTitle>
+              <DialogContent dividers>
+                <DialogContentText>
+                  偵測到新版本，更新中。
+                  New Version Detected, Updating.
+                </DialogContentText>
+              </DialogContent>
+            </Dialog>
+          ) : null}
+
           <Switch>
             <Route path="/lrt">
               <Header />
